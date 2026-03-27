57b6c97130edc547b7afa81dd305d976432a3a99
LeonBUBT <lb70655@ubt-uni.net>
2026-03-28 00:44:40 +0100

    added middleware and extensions

--- server/src/TUTORA.API/Program.cs
+++ server/src/TUTORA.API/Program.cs
@@ -1,23 +1,41 @@
+using TUTORA.API.Extensions;
+using TUTORA.API.Middleware;
+using TUTORA.Infrastructure.Extensions;
+using TUTORA.Infrastructure.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddJwtAuthentication();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        var origins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>()
+                      ?? ["http://localhost:3000"];
+        policy.WithOrigins(origins)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+    await DataSeeder.SeedAsync(scope.ServiceProvider);
 }
 
-app.UseHttpsRedirection();
-
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors("AllowFrontend");
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
